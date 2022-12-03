dcc3ec7fdb1c5b9def9287497ef00d56ced71bc5
Thoriq As <72277295+Muhthoriqas@users.noreply.github.com>
2022-12-04 02:33:57 +0800

    Update Using API

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from newspaper import Article
+from newsapi import NewsApiClient
+import pandas as pd
 import joblib
 
 
@@ -8,10 +10,6 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
-
-@app.route('/newsfeed')
-def news_feed():
-    return render_template('news_feed.html')
 
 @app.route('/aboutus')
 def about_us():
@@ -37,12 +35,11 @@ def result():
         return redirect('/')
 
 
+loaded_model = joblib.load('model.sav')
+tfidf_vectorizer = joblib.load('vectorizer.sav')
 
 def checkFakeNews(datas):
     data = [datas]
-    loaded_model = joblib.load('model.sav')
-    tfidf_vectorizer = joblib.load('vectorizer.sav')
-
     tfidf_test = tfidf_vectorizer.transform(data)
     result = loaded_model.predict(tfidf_test)
 
@@ -72,6 +69,51 @@ def getImage(url):
     image = article.top_image
     return image
 
+
+newsapi=NewsApiClient(api_key='e1155f7ec34e4af690db87f96fec058a')
+
+loaded_model=joblib.load('model.sav')
+tfidf_vectorizer=joblib.load('vectorizer.sav')
+
+def predict_fake(title,text):
+
+    data={"Unnamed: 0":["0000"], "title":[title], "text":[text], "label":["FAKE/REAL"]}
+    frame=pd.DataFrame(data, columns= ["Unnamed: 0", "title","text","label"])
+    frame.drop("label",axis=1)
+    tfidf_test=tfidf_vectorizer.transform(frame['text'])
+    pred = loaded_model.predict(tfidf_test)
+    return pred[0]
+
+def predict(url):
+
+    try:
+        article=Article(url)
+        article.download()
+        article.parse()
+
+        article.nlp()
+
+        return [str(article.title), predict_fake(str(article.title),str(article.text)),str(article.summary),article.top_image]
+    except ValueError:
+        return("Something went wrong")
+    finally:
+        return  [str(article.title), predict_fake(str(article.title), str(article.text)),  str(article.summary),article.top_image]
+
+
+def get_headlines():
+    final=[]
+    top_headlines= newsapi.get_top_headlines(language='en',page_size=6)
+
+    for i in top_headlines['articles']:
+        k=predict(i['url'])
+        date =i['publishedAt']
+        final.append([i['url'], i['title'], i['description'], i['source']['name'], i['urlToImage'], k[1].upper(),date[0:10]])
+    return final
+
+@app.route('/newsfeed')
+def news_feed():
+    headlines=get_headlines()
+    return render_template('news_feed.html',headlines=headlines)
 
 if __name__== '__main__':
     app.run(debug=True)
